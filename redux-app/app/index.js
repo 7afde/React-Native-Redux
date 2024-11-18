9bc62d188c961fff3700e508ec654629703e07e4
hafed <hafed.zouyed@univ-constantine2.dz>
2024-11-18 21:21:37 +0100

    adding home screen

---
+++ redux-app/app/index.js
@@ -0,0 +1,83 @@
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { router } from "expo-router";
+
+const Home = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.overlay}>
+        <Text style={styles.mainText}>Masynctech</Text>
+        <Text style={styles.subText}>Coding School</Text>
+        <Text style={styles.tagline}>Build Apps, Build Futures</Text>
+      </View>
+      <View style={styles.buttons}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => router.push("/auth/login")}
+        >
+          <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => router.push("/auth/register")}
+        >
+          <Text style={styles.buttonText}>Register</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+export default Home;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  overlay: {
+    ...StyleSheet.absoluteFillObject,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+  },
+  mainText: {
+    color: "white",
+    fontSize: 68,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  subText: {
+    color: "white",
+    fontSize: 24,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  tagline: {
+    color: "white",
+    fontSize: 18,
+    fontStyle: "italic",
+    textAlign: "center",
+    marginTop: 10,
+  },
+  buttons: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+    position: "absolute",
+    bottom: 30,
+    left: 0,
+    right: 0,
+  },
+  button: {
+    backgroundColor: "#6200ea",
+    paddingVertical: 12,
+    paddingHorizontal: 20,
+    borderRadius: 25,
+    elevation: 3, // Adds a shadow effect on Android
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+});
