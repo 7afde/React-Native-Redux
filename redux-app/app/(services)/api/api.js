bc804b151cd81d9e9b10404d1a4ce2c28b27d379
hafed <hafed.zouyed@univ-constantine2.dz>
2024-11-21 18:41:27 +0100

    adding react query and login/register api

---
+++ redux-app/app/(services)/api/api.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+// Login
+const loginUser = async (email, password) => {
+  const response = await axios.post(
+    "https://red-eggs-begin.loca.lt/api/users/login",
+    { email, password }
+  );
+  return response.data;
+};
+
+// Register
+const registerUser = async (email, password) => {
+  const response = await axios.post(
+    "https://red-eggs-begin.loca.lt/api/users/register",
+    { email, password }
+  );
+  return response.data;
+};
+
+export { loginUser, registerUser };
--- redux-app/app/_layout.js
+++ redux-app/app/_layout.js
@@ -1,12 +1,16 @@
+import { QueryClientProvider } from "@tanstack/react-query";
 import { Stack } from "expo-router";
+import queryClient from "./(services)/queryClient";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen
-        name="index"
-        options={{ title: "Home", headerShown: false }}
-      />
-    </Stack>
+    <QueryClientProvider client={queryClient}>
+      <Stack>
+        <Stack.Screen
+          name="index"
+          options={{ title: "Home", headerShown: false }}
+        />
+      </Stack>
+    </QueryClientProvider>
   );
 }
